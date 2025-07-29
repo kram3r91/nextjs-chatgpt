82c9a719bd5e149d697c8b7cf4168997ab5befdf
Claudiu-Ciprian Todiras <claudiut-v@herbalife.com>
2025-07-29 14:44:31 +0300

    Init auth0 & configure project

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import Head from "next/head";
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
+  const { isLoading, error, user } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
   return (
-    <div>
+    <>
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>NextJS & ChatGPT - Login or Signup</title>
       </Head>
-      <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
-    </div>
+      <div className="min-h-screen w-full bg-gray-800 text-center text-white">
+        {!!user && <Link href="/api/auth/logout">Logout</Link>}
+        {!user && <Link href="/api/auth/login">Login</Link>}
+      </div>
+    </>
   );
 }
